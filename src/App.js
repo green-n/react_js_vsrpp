e6f07f650b45321414ff07e9b7fe59d8db67e89f
green-n <romunuk@gamil.com>
2022-09-19 21:40:26 +0300

    first route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import LogIn from './pages/LogIn';
 import Register from './pages/Register';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   const [state,setState] = useState([])
@@ -10,10 +11,12 @@ function App() {
     setState(state)
   }
   return (<>
-
-      <UserManagementPage state={state} returnInfo = {getStateFromComponent}/>
-      {/* <LogIn state={state}/> */}
-      <Register state={state} returnInfo = {getStateFromComponent}/>
+      <Routes>
+        <Route path ='/logIn' element={<LogIn state={state}/>}/>
+      </Routes>
+      {/* <UserManagementPage state={state} returnInfo = {getStateFromComponent}/>
+      
+      <Register state={state} returnInfo = {getStateFromComponent}/> */}
     </>
    );
 }

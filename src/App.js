3fec2be52c7d281e50fc61b47130e565345d3b6c
green-n <65806207+green-n@users.noreply.github.com>
2022-09-20 11:12:48 +0000

    link test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import LogIn from './pages/LogIn';
 import Register from './pages/Register';
-import {Routes, Route} from 'react-router-dom'
+import {Routes,Link, Route} from 'react-router-dom'
 
 function App() {
   const [state,setState] = useState([])
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path ='/logIn' element={<LogIn state={state}/>}/>
       </Routes>
+      {/* <Link to={{pathname: "/logIn"}} >take</Link> */}
       {/* <UserManagementPage state={state} returnInfo = {getStateFromComponent}/>
       
       <Register state={state} returnInfo = {getStateFromComponent}/> */}
